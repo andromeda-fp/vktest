55dede2c2b044cbf814ee815848e3d5759c93c77
andromeda <andromeda@lenovo>
2026-08-17 20:06:22 +0200

    RGFW

---
+++ RGFW.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE CApiFFI #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UnboxedTuples #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module RGFW where
+
+import HsBindgen.Runtime.LibC
+import HsBindgen.TH
+
+let
+  conf = def{clang = def{extraIncludeDirs = [Dir "include"]}}
+  confTH = def{verbosity = Verbosity Warning}
+ in
+  withHsBindgen conf confTH $ hashInclude "RGFW.h"
--- shell.nix
+++ shell.nix
@@ -1,13 +1,25 @@
 let
   pkgs = import <nixpkgs> {};
 in
-pkgs.mkShellNoCC {
+pkgs.mkShell {
   packages = [
     pkgs.cabal-install
     pkgs.ghc
+    pkgs.llvm
+    pkgs.libclang
+    pkgs.pkg-config
     pkgs.vulkan-headers
     pkgs.vulkan-loader
-    pkgs.pkg-config
   ];
-  LIBRARY_PATH = "${pkgs.vulkan-loader}/lib";
+  buildInputs = [
+    pkgs.libllvm
+    pkgs.vulkan-headers
+    pkgs.vulkan-loader
+  ];
+  shellHook = ''
+    BINDGEN_EXTRA_CLANG_ARGS="$(<${pkgs.clang}/nix-support/cc-cflags) $(<${pkgs.clang}/nix-support/libc-cflags) $NIX_CFLAGS_COMPILE"
+    export BINDGEN_EXTRA_CLANG_ARGS
+    BINDGEN_BUILTIN_INCLUDE_DIR=disable
+    export BINDGEN_BUILTIN_INCLUDE_DIR
+  '';
 }
